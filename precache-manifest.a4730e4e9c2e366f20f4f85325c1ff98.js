ebd8356127016c76ebbfef3bcd2cd68963de11a2
WihanBarnard <wbarnard@alphawave.co.za>
2020-04-21 14:42:16 +0200

    Updates

---
+++ precache-manifest.a4730e4e9c2e366f20f4f85325c1ff98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54ea7bdb67ea1928f570b0f6b439fc7",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "27a20b786eb4403f2bcd",
+    "url": "/react-calc/static/css/main.337ce2d9.chunk.css"
+  },
+  {
+    "revision": "bb2f286decff8a8e373c",
+    "url": "/react-calc/static/js/2.2236007e.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/react-calc/static/js/2.2236007e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a20b786eb4403f2bcd",
+    "url": "/react-calc/static/js/main.d21cadc0.chunk.js"
+  },
+  {
+    "revision": "405f260ce92dbfe4e7d0",
+    "url": "/react-calc/static/js/runtime-main.2ae973cd.js"
+  }
+]);
